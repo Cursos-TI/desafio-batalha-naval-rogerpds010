a3430adb94721b2834f98618e6e7cf4285df89ba
roger_pereira_ps <rogerps.010@gmail.com>
2025-04-02 06:49:43 +0000

    add nível intermediário

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,11 +4,14 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-void imprimirTabuleiro(char tabuleiro[10][10]) {
+#define tamanho 10
+#define navio 3
+
+void imprimirTabuleiro(char tabuleiro[tamanho][tamanho]) {
     printf("   A B C D E F G H I J\n"); // Cabeçalho das colunas
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < tamanho; i++) {
         printf("%2d ", i + 1); // Índice das linhas
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < tamanho; j++) {
             printf("%c ", tabuleiro[i][j]);
         }
         printf("\n");
@@ -51,30 +54,39 @@ int main() {
     printf(" Tabuleiro Batalha naval \n");
 
 
-    char tabuleiro[10][10];
+    char tabuleiro[tamanho][tamanho];
     
     // Inicializa o tabuleiro com água ('~')
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
             tabuleiro[i][j] = '~';
         }
     }
     // Declaração e posicionamento dos navios
-    int navio1_x = 2, navio1_y = 3; // Navio horizontal começando em (3,4)
+    int navio1_x = 3, navio1_y = 4; // Navio horizontal começando em (4,5)
     int navio2_x = 5, navio2_y = 6; // Navio vertical começando em (6,7)
-    
+    int navio3_x = 0, navio3_y = 0; // Navio diagonal começando em (1,1) 
+    int navio4_x = 9, navio4_y = 5; // Navio diagonal começando em (10,6)
     // Posiciona o navio horizontalmente
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < navio; i++) {
         tabuleiro[navio1_x][navio1_y + i] = '3';
     }
     
     // Posiciona o navio verticalmente
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < navio; i++) {
         tabuleiro[navio2_x + i][navio2_y] = '3';
     }
-    
+    // Posiciona o navio diagonalmente
+    for (int i = 0; i < navio; i++) {
+        tabuleiro[navio3_x + i][navio3_y + i] = '3';
+    }
+    // Posiciona o navio diagonalmente
+    for (int i = 0; i < navio; i++) {
+        tabuleiro[navio4_x - i][navio4_y - i] = '3';
+    // o - faz com que o navio esteja de costas
+    }
     imprimirTabuleiro(tabuleiro);
-    
+
 
 
     return 0;
